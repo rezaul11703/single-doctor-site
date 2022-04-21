9a5da0b6e81508d7508b3ed8209452c977214e57
rezaul11703 <rezaulkarim11703@gmail.com>
2022-04-22 00:14:26 +0600

    header section completed without logo image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Pages/Sharing/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Appoinment from './Pages/Appoinment/Appoinment';
+import LogIn from './Pages/Log In/LogIn';
+import Register from './Pages/Register/Register';
+import About from './Pages/About/About';
+import Blog from './Pages/Blog/Blog';
+import Footer from './Pages/Sharing/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Container'>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/appoinment' element={<Appoinment></Appoinment>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/blog' element={<Blog></Blog>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/Pages/Sharing/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import "./Header.css"
+import logo from "../../../img/Home_pic.jpg"
+import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+const Header = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand as={Link} to="/"> 
+  <div className='d-flex'>
+  <img src={logo}/> 
+  <p>
+  <span>DMJH</span> 
+  <br/> <small>HEALTH CARE</small>
+  </p>
+  </div>
+  </Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="ms-auto">
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link  as={Link} to="appoinment">
+        Appoinment
+      </Nav.Link>
+      <Nav.Link as={Link} to="login">Log In</Nav.Link>
+      <Nav.Link  as={Link} to="register">
+        Register
+      </Nav.Link>
+      <Nav.Link as={Link} to="about">About</Nav.Link>
+      <Nav.Link as={Link} to="blog">
+        Blog
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+  );
+};
+
+export default Header;
