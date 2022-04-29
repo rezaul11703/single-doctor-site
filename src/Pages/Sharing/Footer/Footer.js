117dfb83358ead0a864492296f5b4be56acb1d5f
rezaul11703 <rezaulkarim11703@gmail.com>
2022-04-29 14:59:26 +0600

    home page responsive done

--- src/Pages/Sharing/Footer/Footer.js
+++ src/Pages/Sharing/Footer/Footer.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import { Col, Container, Navbar, Row } from 'react-bootstrap';
 import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className='fixed-bottom mt-5' >
       <Navbar bg="dark">
-    <Container >
-    <Navbar.Brand className="text-white fs-6" > Contact Us : # 120,Dhanmondi gate, Dhaka -1216 , Mobile No: 07117459006   </Navbar.Brand>
-      <Navbar.Brand className="text-white fs-6" > Copright @ {new Date().getFullYear()}  All right reserves to DMJH Health Point  </Navbar.Brand>
+    <Container  >
+    <Row>
+    <Col sm><Navbar.Brand className="text-white fs-6 text-center" > Contact Us : # 120,Dhanmondi gate, Dhaka -1216 , Mobile No: 07117459006   </Navbar.Brand>
+    </Col>
+    <Col sm>
+    <Navbar.Brand className="text-white fs-6 text-center" > Copright @ {new Date().getFullYear()}  All right reserves to DMJH Health Point  </Navbar.Brand>
+    </Col>
+  </Row>
+    
+      
       
     </Container>
   </Navbar>
